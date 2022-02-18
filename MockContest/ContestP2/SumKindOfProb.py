d25ebb63274055d1915605536c3f78803ac1de67
Matthew Fernandez <matthew9erfernandez@gmail.com>
2022-02-17 23:15:52 -0800

    Passes all test cases

--- MockContest/ContestP2/SumKindOfProb.py
+++ MockContest/ContestP2/SumKindOfProb.py
@@ -1,22 +1,15 @@
-import numpy as np
-
 lines = int(input())
 allVals = []
-tempVals = []
-cumsum = []
-# predefined 
-cumsum = np.cumsum(np.arange(0,10000,1)) 
-oddSum = np.cumsum(np.arange(1,10000,2)) 
-evenSum = np.cumsum(np.arange(2,10000,2)) 
-
-for i in range(lines):
-    values = [int(x) for x in input().split() ]
+for i in range(1,lines+1):
+    vals = [int(x) for x in input().split()]
     sumList = []
-    sumList.append(values[0])
-    sumList.append(cumsum[values[1]])
-    sumList.append(oddSum[values[1]-1])
-    sumList.append(evenSum[values[1]-1])
+    sumList.append(vals[0])
+    # summation formula
+    sumList.append( int( ( (vals[1]) * ( (vals[1])+1) ) / 2 ) ) # all positive 
+    sumList.append( int( (vals[1]) * (vals[1]) ) ) # odd
+    sumList.append( int( (vals[1]) * ( (vals[1]) + 1 ) ) ) # even
     allVals.append(sumList)
 
+
 for i in range(len(allVals)):
-    print(allVals[i][0], " ",allVals[i][1], " ",allVals[i][2], " ",allVals[i][3])
+    print(allVals[i][0],allVals[i][1],allVals[i][2], allVals[i][3])
