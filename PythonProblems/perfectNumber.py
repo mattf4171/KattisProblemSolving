ba4e29b611e3b6281f41ed9feec2fab9ed298cd8
Matthew Fernandez <matthew9erfernandez@gmail.com>
2023-01-09 23:43:19 -0800

    Pass all test cases

--- PythonProblems/perfectNumber.py
+++ PythonProblems/perfectNumber.py
@@ -21,15 +21,15 @@
 
 def perfect_number(num):
     
-    if num<=1:
+    if num <= 1:
         return False
     
     divisors = set([1])
     
-    for i in range(2,int(num**0.5)+1):   # from 2 to num**0.5
-        if num%i==0:
+    for i in range(2, int(num**0.5) + 1):   # from 2 to num**0.5
+        if num % i == 0:
             divisors.add(i)
-            divisors.add(num//i)
+            divisors.add(num // i)
 
     return sum(divisors)==num
 
