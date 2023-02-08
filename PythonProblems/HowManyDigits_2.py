1dfb1b4d7bc579e5391a523d905a4c3496a64d61
Matthew Fernandez <33529858+mattf4171@users.noreply.github.com>
2023-02-08 14:15:39 -0800

    Update HowManyDigits comments

--- PythonProblems/HowManyDigits_2.py
+++ PythonProblems/HowManyDigits_2.py
@@ -27,16 +27,13 @@ if __name__ == "__main__":
 	values.append(1) # digits needed for inputs: 0 & 1
 	digits = 0
 	for i in range(2, 1000001):
-		# print('Digits: ',digits)
 		digits += math.log10(i)
 		values.append(math.floor(digits) + 1)
-	# print(values[:20])
 
 	input_values = []
 	# collect all user input values 
 	for i in sys.stdin:
 		x = eval(i)
-		# x = [eval(j) for j in x] # cast list of str to int
 		input_values.append(x)
 
 	# get digit count using pre-defined list of digits required
